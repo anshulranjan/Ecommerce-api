207eed0f6e52416f56aaa6dd20fe032b8e5e962b
anshulranjan <ranjananshul1@gmail.com>
2021-09-19 20:15:18 +0530

    Get token and create and update user and send the response

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,23 @@
-exports.CreateorUpdateUser = (req,res) => {
-    res.json({
-        data:"HELLO TO NODE JS",
-    });
+const User = require("../models/user");
+
+exports.CreateorUpdateUser = async (req, res) => {
+  const { name, picture, email } = req.user;
+
+  const user = await User.findOneAndUpdate(
+    { email },
+    { name: email.split("@")[0], picture },
+    { new: true }
+  );
+  if (user) {
+    console.log("USER UPDATED", user);
+    res.json(user);
+  } else {
+    const newUser = await new User({
+      email,
+      name: email.split("@")[0],
+      picture,
+    }).save();
+    console.log("USER CREATED", newUser);
+    res.json(newUser);
+  }
 };
