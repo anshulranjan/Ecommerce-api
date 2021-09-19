07769f7678d7755d0b14bb2298cd946d1559c57d
anshulranjan <ranjananshul1@gmail.com>
2021-09-19 16:51:28 +0530

    Get AuthToken from Frontend

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express')
+const {authCheck} = require("../middlewares/auth") 
 const router = express.Router()
 const {CreateorUpdateUser} = require('../controllers/auth')
-router.get('/create-update-user', CreateorUpdateUser)
+router.post('/create-update-user', authCheck, CreateorUpdateUser)
 
 module.exports = router;
