22441f49679d26edaec7e30f9f365254d564288c
anshulranjan <ranjananshul1@gmail.com>
2021-09-20 19:08:52 +0530

    Admin Check route

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,5 @@
 const admin = require("../firebase");
-
+const User = require('../models/user')
 exports.authCheck = async (req, res, next) => {
   // console.log(req.headers); // token
   try {
@@ -13,5 +13,19 @@ exports.authCheck = async (req, res, next) => {
     res.status(401).json({
       err: "Invalid or expired token",
     });
+  }
+};
+
+exports.adminCheck = async(req,ress,next) => {
+  const {email} = req.user;
+  const adminUser = await User.findOne({email}).exec()
+  if(adminUser.role !== 'admin')
+  {
+    res.status(403).json({
+      err: 'Admin Resource. Access Denied'
+    });
+  }
+  else{
+    next();
   }
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const {authCheck} = require("../middlewares/auth") 
+const {authCheck, adminCheck} = require("../middlewares/auth") 
 const router = express.Router()
 const {CreateorUpdateUser, currentUser} = require('../controllers/auth')
 router.post('/create-update-user', authCheck, CreateorUpdateUser)
 router.post('/currentuser', authCheck, currentUser)
+router.post('/currentadmin', authCheck, adminCheck, currentUser)
 
 module.exports = router;
